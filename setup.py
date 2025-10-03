598d561eb7dbd305928c2b748bb0380b0c7008bf
nishad2725 <nishadkane@gmail.com>
2025-10-02 21:30:11 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Nishad Kane",
+    author_email="nishadkane@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
